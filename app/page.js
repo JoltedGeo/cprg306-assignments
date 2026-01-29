eaade138f1901f386e91ad541f3807170560da92
Hunter Norman <hunternorman05@gmail.com>
2026-01-29 10:35:09 -0700

    Initial commit

--- app/page.js
+++ app/page.js
@@ -6,7 +6,11 @@ export default function HomePage() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
 
       <Link href="/week-2">
-        Week 2 Assignment
+        -Week 2 Assignment - 
+      </Link>
+
+      <Link href="/week-3">
+        Week 3 Assignment - 
       </Link>
     </main>
   );
