dcdb166aea5865592bdd59b0db15ec0ec3506028
Hunter Norman <hunternorman05@gmail.com>
2026-03-26 21:04:33 -0600

    Added security fix where api key was shown.

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCVxt58Q10x4UnslZH8tXKnJ6OIrX8ZbQw",
-  authDomain: "cprg306-assignments-b28d3.firebaseapp.com",
-  projectId: "cprg306-assignments-b28d3",
-  storageBucket: "cprg306-assignments-b28d3.firebasestorage.app",
-  messagingSenderId: "90234044656",
-  appId: "1:90234044656:web:d61b33570108bb7b30c235"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
